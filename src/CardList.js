2a37577d681125fb1c1de1303366c3c7b439ee18
Petr Kahánek <kahanek.peta@gmail.com>
2020-05-05 16:13:05 +0200

    progress

---
+++ src/CardList.js
@@ -0,0 +1,23 @@
+import React from "react"
+import Card from "./Card.js"
+// import { robots } from "./robots.js"
+
+const CardList = ({ robots }) => {
+    const cardsArray = robots.map((robot, i) => {
+        return (
+            <Card
+                key={i}
+                id={robots[i].id}
+                name={robots[i].name}
+                email={robots[i].email}
+            />
+        );
+    })
+    return (
+        <div>
+            {cardsArray}
+        </div>
+    )
+}
+
+export default CardList
